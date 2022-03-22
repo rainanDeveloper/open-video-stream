a9bd1784f0d8d34464d74b4f1d469766c8587fdd
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-03-21 22:35:54 -0300

    feat(#6): created function to create a bcrypt hash and to transform user dto
    created a function to create a bcrypt hash using enviroment variable, and a function to transform
    the user body
    
    Closes #6

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
+import { User } from '../schemas/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly hashDifficulty: number;
+
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+  ) {
+    this.hashDifficulty = configService.get('HASH_DIFFICULTY') || 12;
+  }
+
+  async createHash(password: string): Promise<string> {
+    const hash = await bcrypt.hashSync(password, this.hashDifficulty);
+
+    return hash;
+  }
+
+  async transformBody(dto: CreateUserDto): Promise<CreateUserDto> {
+    const transformedDto: CreateUserDto = {
+      login: dto.login,
+      email: dto.email,
+      password: '',
+    };
+
+    transformedDto.password = await this.createHash(dto.password);
+
+    return transformedDto;
+  }
+}
