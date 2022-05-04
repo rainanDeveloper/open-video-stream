a8e46c16467fb0fe4e5e34bfbab8369d8343ac84
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-05-04 00:01:30 -0300

    fix(#22): added swagger api tag to UserAuthController

--- src/users/controllers/users-auth.controller.ts
+++ src/users/controllers/users-auth.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { LoginDto } from '../dto/login.dto';
 
 @Controller('users-auth')
+@ApiTags('Users Authentication')
 export class UsersAuthController {
   @Post()
   async login(loginDto: LoginDto) {
